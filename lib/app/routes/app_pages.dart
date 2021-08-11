8dbcdfa6c2b9af3ade31eb9f91059e5099653115
lucas-de-almeida <lucasde.almeida@hotmail.com>
2021-08-11 04:10:26 -0300

    criada estrutura base de pastas e estrutura de rotas

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:login_firebase/app/routes/app_routes.dart';
+import 'package:login_firebase/app/ui/android/home_screen.dart';
+import 'package:login_firebase/app/ui/android/initial_page.dart';
+import 'package:login_firebase/app/ui/android/login_page.dart';
+
+class AppPages {
+  static final routes = [
+    GetPage(name: Routes.INITIAL, page: () => InitialPage()),
+    GetPage(name: Routes.LOGIN, page: () => LoginPage()),
+    GetPage(name: Routes.HOME, page: () => HomePage()),
+  ];
+}
